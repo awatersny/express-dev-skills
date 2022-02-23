5f71929ee3fd83bce04a0429b8dc72a25804dd8d
awatersny <awatersny@gmail.com>
2022-02-23 18:49:23 -0500

    Rename users route to skills

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import logger from 'morgan'
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
-import { router as usersRouter } from './routes/users.js'
+import { router as usersRouter } from './routes/skills.js'
 
 // set up app
 const app = express()
