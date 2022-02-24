6b550439aa311badd6a5604aac5140dc2d3aaed3
awatersny <awatersny@gmail.com>
2022-02-23 22:27:24 -0500

    Render Skills

--- routes/skills.js
+++ routes/skills.js
@@ -1,9 +1,15 @@
 import { Router } from 'express'
+import * as skillDb from "../data/skill-db.js"
 const router = Router()
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource')
+  skillDb.find({}, function(error, skills) {
+  res.render('skills/index', {
+    skills: skills,
+    error: error
+  })
+})
 })
 
 export {
