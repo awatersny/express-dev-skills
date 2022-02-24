20103df78f923da2b54c5d648bd589748318f7c2
awatersny <awatersny@gmail.com>
2022-02-24 09:42:55 -0500

    Initial controller setup

---
+++ controllers/skills.js
@@ -0,0 +1,12 @@
+function index(req, res) {
+  skillDb.find({}, function(error, skills) {
+    res.render('skills/index', {
+      skills: skills,
+      error: error
+    })
+  })
+}
+
+export {
+  index
+}
